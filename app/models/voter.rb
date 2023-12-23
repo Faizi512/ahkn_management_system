01b1a8df8076e6c3fc0a1d68e7c56865fe5d721b
Faizan Haider <hfaizan53@gmail.com>
2023-12-23 20:26:45 +0500

    List updated

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -2,7 +2,12 @@ class Voter < ApplicationRecord
   include PgSearch::Model
   pg_search_scope :search, against: [:cnic, :kid, :name, :father_name],
     using: { tsearch: { any_word: true, prefix: true } }
+  
+  def male
+    Voter.where(cnic: "CAST(cnic AS INTEGER) % 2 = 0")
+  end
 
+  # scope :male, -> { where("LENGTH(cnic) > 0 AND CAST(SUBSTRING(cnic, -1) AS INTEGER) % 2 != 0") }
 
   def unlock
     self.update(printed: false)
