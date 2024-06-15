288663907f0c8d84d5ea6df147a54b8cac3b7d0b
Faizan Haider <hfaizan53@gmail.com>
2024-06-15 15:20:37 +0500

    Rashan Qurban Eid 2024

--- app/services/data_loader_service.rb
+++ app/services/data_loader_service.rb
@@ -11,13 +11,46 @@ class DataLoaderService
 
     (2..spreadsheet.last_row).each do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
-      
+      # byebug
       # Map Excel column names to database column names
+      # mapped_row = {
+      #   "cnic" => row["CNIC"]&.split("-")&.join,
+      #   "kid" => row["KID"],
+      #   "name" => row["NAME"],
+      #   "father_name" => row["FATHER NAME"],
+      #   "age" => row["AGE"],
+      #   "date_of_birth" => row["DOB"],
+      #   "voter_status" => "Voter",
+      #   "created_at" => Time.now,
+      #   "updated_at" => Time.now,
+      #   "disabled" => false,
+      #   "printed" => false,
+      #   "voter_no" => row["VOTER NO"],
+      #   "akhn" => row["AKHN"],
+      #   "verification" => row["VERIFICATION"],
+      #   "execution_no" => row["EXECUTION NO"],
+      #   "f_cnic" => row["F. CNIC"]&.split("-")&.join,
+      #   "spouse_name" => row["SPOUSE NAME"],
+      #   "sp_cnic" => row["SP. CNIC"]&.split("-")&.join,
+      #   "qaber" => row["QABER"],
+      #   "address" => row["ADDRESS"],
+      #   "city" => row["CITY"],
+      #   "cell_no" => row["cell no"],
+      #   "mobile" => row["MOBILE #"],
+      #   "cnic_chk" => row["CNIC CHK"]&.split("-")&.join,
+      #   "qabeela" => row["QABEELA"],
+      #   "urfiat" => row["URFIAT"],
+      #   "wf_upto" => row["W/F UPTO"],
+      #   "family_no" => row["FAMULY NO"],
+      #   "dob" => row["DOB"],
+      #   "kid_chk" => row["KID CHK"]
+      # }
+      # byebug
       mapped_row = {
         "cnic" => row["CNIC"]&.split("-")&.join,
-        "kid" => row["KID"],
-        "name" => row["NAME"],
-        "father_name" => row["FATHER NAME"],
+        "kid" => row["CndID"],
+        "name" => row["Name"],
+        "father_name" => row["FName"],
         "age" => row["AGE"],
         "date_of_birth" => row["DOB"],
         "voter_status" => "Voter",
@@ -35,15 +68,15 @@ class DataLoaderService
         "qaber" => row["QABER"],
         "address" => row["ADDRESS"],
         "city" => row["CITY"],
-        "cell_no" => row["cell no"],
-        "mobile" => row["MOBILE #"],
+        "cell_no" => row["MobileNo"],
+        "mobile" => row["MobileNo"],
         "cnic_chk" => row["CNIC CHK"]&.split("-")&.join,
-        "qabeela" => row["QABEELA"],
-        "urfiat" => row["URFIAT"],
-        "wf_upto" => row["W/F UPTO"],
+        "qabeela" => row["QabeelaName"],
+        "urfiat" => row["UrfiatName"],
+        "wf_upto" => row["FWelfareFundNo"],
         "family_no" => row["FAMULY NO"],
         "dob" => row["DOB"],
-        "kid_chk" => row["KID CHK"]
+        "kid_chk" => row["CndID"]
       }
       puts mapped_row
       Voter.create!(mapped_row)
